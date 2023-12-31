b5d35bbee40d0fbc1d2a7bebbbaa4e7024fb42b9
LuiiVuitton <Luis1134@icloud.com>
2023-12-30 23:46:46 -0500

    Navigation bar hamburge menu display

--- MyPortfolioSite/src/components/NavBar.tsx
+++ MyPortfolioSite/src/components/NavBar.tsx
@@ -5,17 +5,30 @@ interface NavBarProps {
 
 function NavBar({ brandName, imageSrcPath }: NavBarProps) {
   return (
-    <nav className="navbar navbar-light bg-light">
-      <a className="navbar-brand" href="#">
-        <img
-          src={imageSrcPath}
-          width="60"
-          height="60"
-          className="d-inline-block align-top"
-          alt=""
-        />
-        {brandName}
-      </a>
+    <nav className="navbar navbar-light bg-white shadow">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          <img
+            src={imageSrcPath}
+            width="60"
+            height="60"
+            className="d-inline-block align-center"
+            alt=""
+          />
+          <span className="fw-bolder fs 4">{brandName}</span>
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+      </div>
     </nav>
   );
 }
