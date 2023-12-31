46732ef58c31c6d06c14777e47dde3230ff217c4
LuiiVuitton <Luis1134@icloud.com>
2023-12-30 22:21:59 -0500

    Started to used (App.tsx) to display (NavBar) inside of the (components) folder. Using Bootstrap to create the Navigation Bar.

---
+++ MyPortfolioSite/src/components/NavBar.tsx
@@ -0,0 +1,18 @@
+function NavBar() {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        <img
+          src="/docs/4.0/assets/brand/bootstrap-solid.svg"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+          alt=""
+        />
+        Bootstrap
+      </a>
+    </nav>
+  );
+}
+
+export default NavBar;
