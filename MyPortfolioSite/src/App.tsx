4313589697d3e6237d3f4e8ce0849dc15a8fc5fd
LuiiVuitton <Luis1134@icloud.com>
2024-01-01 15:10:50 -0500

    Navigation bar Front end, using Bootstrap

--- MyPortfolioSite/src/App.tsx
+++ MyPortfolioSite/src/App.tsx
@@ -1,6 +1,8 @@
 import NavBar from "./components/NavBar";
 import "bootstrap/dist/css/bootstrap.css";
 import imagePath from "./assets/react.svg";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function App() {
   return (
--- MyPortfolioSite/src/components/NavBar.tsx
+++ MyPortfolioSite/src/components/NavBar.tsx
@@ -5,7 +5,7 @@ interface NavBarProps {
 
 function NavBar({ brandName, imageSrcPath }: NavBarProps) {
   return (
-    <nav className="navbar navbar-light bg-white shadow">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
           <img
@@ -15,7 +15,6 @@ function NavBar({ brandName, imageSrcPath }: NavBarProps) {
             className="d-inline-block align-center"
             alt=""
           />
-          <span className="fw-bolder fs 4">{brandName}</span>
         </a>
         <button
           className="navbar-toggler"
@@ -28,6 +27,68 @@ function NavBar({ brandName, imageSrcPath }: NavBarProps) {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="#">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="#">
+                Projects
+              </a>
+            </li>
+
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <ul className="dropdown-menu">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Action
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Another action
+                  </a>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Something else here
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
       </div>
     </nav>
   );
