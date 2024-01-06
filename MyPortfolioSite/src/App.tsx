b4b07357486846a25bb4bb042d90841a7e78ac75
LuiiVuitton <Luis1134@icloud.com>
2024-01-05 19:41:22 -0500

    starting homepage

--- MyPortfolioSite/src/App.tsx
+++ MyPortfolioSite/src/App.tsx
@@ -1,11 +1,10 @@
 import NavBar from "./components/NavBar";
 import "bootstrap/dist/css/bootstrap.css";
 import imagePath from "./assets/react.svg";
-import imagePath2 from "./assets/C2A_LOGO.png";
+import imagePath2 from "./assets/MyImage.png";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import Home from "./pages/Index/Home";
-
 function App() {
   return (
     <div>
--- MyPortfolioSite/src/pages/Index/Home.tsx
+++ MyPortfolioSite/src/pages/Index/Home.tsx
@@ -1,20 +1,36 @@
-// NewComponent.tsx
-import React from "react";
+// Home.tsx
+
 interface HomeProps {
   imageSrcPath: string;
 }
 
 function Home({ imageSrcPath }: HomeProps) {
   return (
-    <div>
-      <div className="text-center">
-        <img
-          src={imageSrcPath}
-          width="200"
-          height="200"
-          className="d-inline-block align-center"
-          alt=""
-        />
+    <div className="container mt-5">
+      <div className="row align-items-center">
+        <div className="col-md-6">
+          <img
+            src={imageSrcPath}
+            className="img-fluid rounded-circle"
+            alt="Profile"
+          />
+        </div>
+        <div className="col-md-6 mt-4 mt-md-0">
+          <h1 className="display-4 mb-3">Hello, I'm Luis Ventura</h1>
+          <p className="lead">
+            I'm a passionate Front-End Developer with a love for creating
+            innovative and user-friendly web applications.
+          </p>
+          <p>Let's work together and create something amazing!</p>
+          <div className="mt-4">
+            <a href="#contact" className="btn btn-primary me-3">
+              Contact Me
+            </a>
+            <a href="#projects" className="btn btn-outline-secondary">
+              View Projects
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
